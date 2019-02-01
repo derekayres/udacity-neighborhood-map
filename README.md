3e8c74b696ca69ec763b453f68a3008820173d13
Derek Ayres <wunderlad@Hotmail.com>
2019-02-01 17:54:05 -0500

    Inital set up withReact, Google API, & Foursquare API.

--- README.md
+++ README.md
@@ -66,3 +66,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+CREDITS
+Tutorial videos 1-5 on YouTube by Elharony - Using React, GoogleMap, & Foursquare to load map & data without outside components.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
 componentDidMount() {
-  this.renderMap()
+  this.getVenues()
 }
 
 renderMap = () => {
@@ -13,12 +17,56 @@ renderMap = () => {
   window.initMap = this.initMap
 }
 
+getVenues = () => {
+  const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+  const parameters = {
+    client_id: "JS4S3FPF54JTMCQBK3MZ2PTVSIT4RM4ZDW1J4EA0OJXYDP5V",
+    client_secret: "PYCRGI34TRVTA5LS1Z3J02DCRHQHNHKYOUABJP5DP1TMA2YY",
+    query: "food",
+    ll: "39.926381, -75.155141",
+    v: "20180323"
+  }
+  //  key note to self:  renderMap doesn't execute until the dynamic data comes in from the AJAX request to avoid an empty array.  it comes in as a callback after the data loads.
+  axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items
+      }, this.renderMap())
+    })
+    .catch(error => {
+      console.log("Error." + error)
+    })
+  }
+
   initMap = () => {
+
+          //create a map
           var map = new window.google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-          });
-        }
+            center: {lat: 39.926381, lng: -75.155141},
+            zoom: 14
+          })
+
+          //create info window
+          var infowindow = new window.google.maps.InfoWindow()
+          //display markers
+          this.state.venues.map(myVenue => {
+
+            var contentString = `${myVenue.venue.name} Address: ${myVenue.venue.location.address}`
+            //create marker
+            var marker = new window.google.maps.Marker({
+              position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+              map: map,
+              title: myVenue.venue.name
+            })
+            //click on marker
+            marker.addListener('click', function() {
+              //change the content
+              infowindow.setContent(contentString)
+              //opens an infowindow
+              infowindow.open(map, marker);
+            })
+          })
+  }
 
   render() {
     return (
